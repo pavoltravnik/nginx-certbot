3b95026cce6a155eeaae044f1b7625239c5f8d56
pavoltravnik <pavol.travnik@protonmail.com>
2019-02-22 21:48:17 +0100

    Update app.js

--- node/app.js
+++ node/app.js
@@ -4,6 +4,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api/a', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
